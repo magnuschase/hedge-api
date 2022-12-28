4ff983c4ef9d2e0994b1e49235648edbe6c053fe
magnuschase <jakub@kapala.xyz>
2022-12-28 01:05:48 +0100

    fix: load_custom_model function

--- functions/load_model.py
+++ functions/load_model.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
-
+import os
 import pytorch_lightning as pl
 
 # Import the YOLOv5 model architecture
@@ -15,11 +15,18 @@ import requests
 def load_custom_model(url):
     # Make an HTTP GET request to the URL and save the response
     response = requests.get(url)
+    temp_path = f'./models/temp-{url.split("/")[-1]}'
+
+    # Save the model weights to a temporary file
+    open(temp_path, 'wb').write(response.content)
 
     # Load the YOLOv5 model with the custom weights
-    model = torch.hub.load('ultralytics/yolov5')
-    model.load_state_dict(torch.load(response.content))
+    model = torch.hub.load('./yolov5', 'custom',
+                           path=temp_path, source='local')
     model.conf = 0.5  # confidence threshold (0-1)
+
+    # Remove the temp file
+    os.remove(temp_path)
 
     # Return the model
     return model
@@ -29,7 +36,6 @@ def load_local_model(weights_path):
     # Load the YOLOv5 model with the custom weights
     model = torch.hub.load('./yolov5', 'custom',
                            path=f'./models/{weights_path}', source='local')
-    # model.load_state_dict(torch.load(weights_path))
     model.conf = 0.5  # confidence threshold (0-1)
 
     # Return the model
